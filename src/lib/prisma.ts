5dba288bdcb7a3051e1d7b0f984a74df82b69c72
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-10 16:43:25 +0700

    first commit

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+export class PrismaSingleton {
+    private static prisma: PrismaClient;
+    static getClient() {
+        if (!this.prisma) {
+            this.prisma = new PrismaClient();
+        }
+        return this.prisma;
+    }
+}
