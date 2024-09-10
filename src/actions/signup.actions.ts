010d33fa87660034a0890b42f2896ac19daeac33
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-10 22:50:05 +0700

    feat: 🎸 sign up

--- src/actions/signup.actions.ts
+++ src/actions/signup.actions.ts
@@ -1,5 +1,58 @@
 "use server";
 
-export async function signup(formData: FormData) {
+import { FormState, SignupFormSchema } from "@/lib/definitions";
+import { PrismaSingleton } from "@/lib/prisma";
+import bcrypt from 'bcrypt';
+import { redirect } from "next/navigation";
 
+const prisma = PrismaSingleton.getClient();
+
+const ENDPOINT_LOGIN = "http://localhost:3000/auth/sign-in";
+
+export async function signup(state: FormState, formData: FormData) {
+    const uri = new URL(ENDPOINT_LOGIN);
+
+    const validatedFields = SignupFormSchema.safeParse({
+        username: formData.get('username'),
+        email: formData.get('email'),
+        password: formData.get('password'),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+        }
+    }
+
+    const { username, email, password } = validatedFields.data;
+
+    const existingUser = await prisma.user.findFirst({
+        where: {
+            OR: [
+                { username: username },
+                { email: email }
+            ]
+        }
+    });
+
+    if (existingUser) {
+        return  {
+            errors: {
+                username: existingUser.username === username ? ["Username already taken"] : [],
+                email: existingUser.email === email ? ["Email already taken"] : [],
+            }
+        }
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const data = await prisma.user.create({
+        data: {
+            username: username,
+            email: email,
+            password: hashedPassword
+        }
+    });
+
+    redirect(uri.toString());
 }
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,29 +1,59 @@
+"use client";
+
 import { signup } from "@/actions/signup.actions";
+import { useFormState, useFormStatus } from "react-dom";
 
 export default function SignUpForm() {
+    const [state, action] = useFormState(signup, undefined);
+
+
     return (
         <div>
-            <form action={signup} className="max-w-sm mx-auto">
+            <form action={action} className="max-w-sm mx-auto">
             <div className="mb-5">
                 <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                <input type="text" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             </div>
+            {state?.errors?.username && <p>{state.errors.username}</p>}
+
             <div className="mb-5">
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="abc@gmail.com" required />
+                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="abc@gmail.com" required />
             </div>
+            {state?.errors?.email && <p>{state.errors.email}</p>}
+
             <div className="mb-5">
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             </div>
-            <div className="flex items-start mb-5">
-                <div className="flex items-center h-5">
-                <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                </div>
-                <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-            </div>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            {
+                state?.errors?.password && (
+                    <div>
+                        <p>Password must: </p>
+                        <ul>
+                            {
+                                state.errors.password.map((error) => {
+                                    return (
+                                        <li key={error}>- {error}</li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                )
+            }
+            <SubmitButton/>
             </form>
         </div>
     );
+}
+
+function SubmitButton() {
+    const { pending } = useFormStatus();
+
+    return (
+        <button disabled={pending} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Submit
+        </button>
+    )
 }
